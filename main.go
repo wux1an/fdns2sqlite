a7ede8b807632b84ef075513a8744fb9ba72f185
wux1an <wux1an@outlook.com>
2021-10-21 21:30:56 +0800

    feat: release v1

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Record of fdns
+type Record struct {
+	TimestampString string    `json:"timestamp" gorm:"-"`
+	Timestamp       time.Time `gorm:"timestamp"`
+	Name            string    `json:"name" gorm:"name"`
+	Type            string    `json:"type" gorm:"type"`
+	Value           string    `json:"value" gorm:"value"`
+}
+
+var (
+	input  string
+	output string
+)
+
+func main() {
+	// parse arg
+	if len(os.Args) < 2 {
+		fmt.Printf("Convert fdns.json to sqlite format. Large files will run slowly.\n"+
+			"Usage: %s fdns.json\n", filepath.Base(os.Args[0]))
+		return
+	}
+
+	input = os.Args[1]
+	output = strings.TrimSuffix(filepath.Base(input), path.Ext(input))
+
+	for i := 0; ; i++ {
+		var name = output
+		if i != 0 {
+			name += "-" + strconv.Itoa(i)
+		}
+		name += ".sqlite3"
+
+		if _, err := os.Open(name); os.IsNotExist(err) {
+			output = name
+			break
+		}
+	}
+
+	// init database
+	db, err := gorm.Open(sqlite.Open(output), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_ = db.AutoMigrate(&Record{})
+
+	data, err := os.ReadFile(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var (
+		lines       = strings.Split(strings.TrimSpace(string(data)), "\n")
+		batchSize   = 100
+		batchNum    = len(lines) / batchSize
+		batchRemain = len(lines) % batchSize
+	)
+
+	for i := 0; i < batchNum; i++ {
+		batch := make([]Record, batchSize)
+		for j := 0; j < len(batch); j++ {
+			var record Record
+			err := json.Unmarshal([]byte(lines[i*batchSize+j]), &record)
+			if err != nil {
+				continue
+			}
+			batch[j] = record
+		}
+		db.CreateInBatches(batch, len(batch))
+	}
+
+	batch := make([]Record, batchRemain)
+	for i := 0; i < len(batch); i++ {
+		var record Record
+		err := json.Unmarshal([]byte(lines[batchSize*batchNum+i]), &record)
+		if err != nil {
+			continue
+		}
+		batch[i] = record
+	}
+	db.CreateInBatches(batch, len(batch))
+}
